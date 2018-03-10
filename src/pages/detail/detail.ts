ee602f7f8d547105a6bad32e2b657add881866bf
Rafael Mendes <rafael.sm1990@gmail.com>
2018-03-10 20:53:16 -0300

    class Accessory; total price

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,3 +1,4 @@
+import { Accessory } from './../../models/accessory';
 import { NavLifecycles } from './../../utils/ionic/nav/nav-lifecycles';
 import { Car } from './../../models/car';
 import { Component } from '@angular/core';
@@ -10,14 +11,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage implements NavLifecycles {
 
+  private total: number;
+
   public car: Car;
+  public accessories: Accessory[] = [];
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams) {
       this.car = this.navParams.get('car');
+      this.total = this.car.preco;
+      this.accessories = [
+        { name: 'Freio ABS', price: 800 },
+        { name: 'Ar condicionado', price: 1000 },
+        { name: 'MP3 Player', price: 500 }
+      ]
   }
 
-  ionViewDidLoad() {
+  ionViewDidLoad() { }
+
+  get getTotal(): number {
+    return this.total;
+  }
+
+  public refreshPrice(checked, accesory: Accessory) {
+    checked ? this.total += accesory.price 
+      : this.total -= accesory.price;
   }
 
 }
